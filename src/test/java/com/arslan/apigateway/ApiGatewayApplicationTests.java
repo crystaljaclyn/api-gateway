4c02b05a474ba61f72c31d7313ea6171f1397a43
tuannguyenminh040294 <37282910+tuannguyenminh040294@users.noreply.github.com>
2020-08-06 23:24:05 +0700

    first commit

---
+++ src/test/java/com/arslan/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arslan.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
